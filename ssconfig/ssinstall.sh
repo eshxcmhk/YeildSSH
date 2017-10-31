70112d5917908e08b6b7b4fa0886a81db1e44752
eshxcmhk <935057137@qq.com>
2017-10-31 19:26:17 +0800

    Update ssinstall.sh

--- ssconfig/ssinstall.sh
+++ ssconfig/ssinstall.sh
@@ -2,11 +2,17 @@ mkdir shadow
 yum install python-setuptools -y
 easy_install pip
 pip install shadowsocks
+mkdir /etc/shadowsocks
 curl "https://raw.githubusercontent.com/eshxcmhk/YeildSSH/master/ssconfig/shadowsocks.json" -o "shadowsocks.json"
+cp shadowsocks.json /etc/
+curl "https://raw.githubusercontent.com/eshxcmhk/YeildSSH/master/ssconfig/ss.service" -o "shadowsocks.service"
+cp shadowsocks.service /etc/systemd/system/
 firewall-cmd --zone=public --add-port=8042/tcp --permanent
 firewall-cmd --zone=public --add-port=9042/tcp --permanent
 firewall-cmd --zone=public --add-port=18042/tcp --permanent
 firewall-cmd --zone=public --add-port=8042/udp --permanent
 firewall-cmd --zone=public --add-port=9042/udp --permanent
 firewall-cmd --zone=public --add-port=18042/udp --permanent
-ssserver -c ~/shadow/shadowsocks.json -d start
+systemctl enable shadowsocks
+systemctl start shadowsocks
+systemctl status shadowsocks -l
