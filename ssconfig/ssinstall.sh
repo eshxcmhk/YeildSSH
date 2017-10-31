03a7c1d002175fa40bee825123bccefad8639b41
eshxcmhk <935057137@qq.com>
2017-10-31 19:30:27 +0800

    Update ssinstall.sh

--- ssconfig/ssinstall.sh
+++ ssconfig/ssinstall.sh
@@ -7,12 +7,19 @@ curl "https://raw.githubusercontent.com/eshxcmhk/YeildSSH/master/ssconfig/shadow
 cp shadowsocks.json /etc/
 curl "https://raw.githubusercontent.com/eshxcmhk/YeildSSH/master/ssconfig/ss.service" -o "shadowsocks.service"
 cp shadowsocks.service /etc/systemd/system/
+echo "open port 8042/tcp"
 firewall-cmd --zone=public --add-port=8042/tcp --permanent
-firewall-cmd --zone=public --add-port=9042/tcp --permanent
-firewall-cmd --zone=public --add-port=18042/tcp --permanent
+echo "open port 8042/udp"
 firewall-cmd --zone=public --add-port=8042/udp --permanent
+echo "open port 9042/tcp"
+firewall-cmd --zone=public --add-port=9042/tcp --permanent
+echo "open port 9042/udp"
 firewall-cmd --zone=public --add-port=9042/udp --permanent
+echo "open port 18042/tcp"
+firewall-cmd --zone=public --add-port=18042/tcp --permanent
+echo "open port 18042/udp"
 firewall-cmd --zone=public --add-port=18042/udp --permanent
+firewall-cmd --reload 
 systemctl enable shadowsocks
 systemctl start shadowsocks
 systemctl status shadowsocks -l
