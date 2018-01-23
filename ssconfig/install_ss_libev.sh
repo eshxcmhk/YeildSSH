c27dd518c418461a646d7ed7c6f543e0d5d19b19
eshxcmhk <935057137@qq.com>
2018-01-23 15:37:01 +0800

    Update install_ss_libev.sh

--- ssconfig/install_ss_libev.sh
+++ ssconfig/install_ss_libev.sh
@@ -1,11 +1,30 @@
 mkdir shadow
 cd shadow
-yum install python-setuptools -y
-easy_install pip
-pip install shadowsocks
-mkdir /etc/shadowsocks
+yum install git vim -y
+yum install epel-release -y
+yum install gcc gettext autoconf libtool automake make pcre-devel asciidoc xmlto udns-devel libev-devel mbedtls-devel -y
+
+git clone https://github.com/shadowsocks/shadowsocks-libev.git
+cd shadowsocks-libev
+git submodule update --init --recursive
+
+echo 'install libsodium'
+export LIBSODIUM_VER=1.0.13
+wget https://download.libsodium.org/libsodium/releases/libsodium-$LIBSODIUM_VER.tar.gz
+tar xvf libsodium-$LIBSODIUM_VER.tar.gz
+pushd libsodium-$LIBSODIUM_VER
+./configure --prefix=/usr && make
+make install
+popd
+ldconfig
+
+echo 'compile shadowsocks-libev'
+./autogen.sh && ./configure --prefix=/usr && make
+sudo make install
+
+mkdir -p /etc/shadowsocks-libev
 curl "https://raw.githubusercontent.com/eshxcmhk/YeildSSH/master/ssconfig/shadowsocks.json" -o "shadowsocks.json"
-cp shadowsocks.json /etc/shadowsocks/
+cp shadowsocks.json /etc/shadowsocks-libev/
 curl "https://raw.githubusercontent.com/eshxcmhk/YeildSSH/master/ssconfig/ss.service" -o "shadowsocks.service"
 cp shadowsocks.service /etc/systemd/system/
 # service firewalld start
