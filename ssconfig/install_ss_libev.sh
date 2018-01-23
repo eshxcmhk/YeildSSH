1475cba70ed36ad283397b6f6d19045a75ed3046
eshxcmhk <935057137@qq.com>
2018-01-23 15:29:03 +0800

    Create install_ss_libev.sh

---
+++ ssconfig/install_ss_libev.sh
@@ -0,0 +1,29 @@
+mkdir shadow
+cd shadow
+yum install python-setuptools -y
+easy_install pip
+pip install shadowsocks
+mkdir /etc/shadowsocks
+curl "https://raw.githubusercontent.com/eshxcmhk/YeildSSH/master/ssconfig/shadowsocks.json" -o "shadowsocks.json"
+cp shadowsocks.json /etc/shadowsocks/
+curl "https://raw.githubusercontent.com/eshxcmhk/YeildSSH/master/ssconfig/ss.service" -o "shadowsocks.service"
+cp shadowsocks.service /etc/systemd/system/
+# service firewalld start
+# echo "open port 8042/tcp"
+# firewall-cmd --zone=public --add-port=8042/tcp --permanent
+# echo "open port 8042/udp"
+# firewall-cmd --zone=public --add-port=8042/udp --permanent
+# echo "open port 9042/tcp"
+# firewall-cmd --zone=public --add-port=9042/tcp --permanent
+# echo "open port 9042/udp"
+# firewall-cmd --zone=public --add-port=9042/udp --permanent
+# echo "open port 18042/tcp"
+# firewall-cmd --zone=public --add-port=18042/tcp --permanent
+# echo "open port 18042/udp"
+# firewall-cmd --zone=public --add-port=18042/udp --permanent
+# firewall-cmd --reload 
+systemctl enable shadowsocks
+systemctl start shadowsocks
+systemctl status shadowsocks -l
+cd ..
+rm -rf ./shadow
