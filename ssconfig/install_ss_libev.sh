852ff1a62f15e50b9f213969fd54f436d83072c9
eshxcmhk <935057137@qq.com>
2018-01-23 15:58:29 +0800

    Update install_ss_libev.sh

--- ssconfig/install_ss_libev.sh
+++ ssconfig/install_ss_libev.sh
@@ -9,7 +9,7 @@ cd shadowsocks-libev
 git submodule update --init --recursive
 
 echo 'install libsodium'
-export LIBSODIUM_VER=1.0.13
+export LIBSODIUM_VER=1.0.15
 wget https://download.libsodium.org/libsodium/releases/libsodium-$LIBSODIUM_VER.tar.gz
 tar xvf libsodium-$LIBSODIUM_VER.tar.gz
 pushd libsodium-$LIBSODIUM_VER
