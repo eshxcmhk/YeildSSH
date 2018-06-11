d9f72c9b46a2920f45306be346985a0d9454ff2a
eshxcmhk <935057137@qq.com>
2018-06-11 14:58:02 +0800

    Update and rename install_ss_libev.sh to ss_libev_configure.sh

--- ssconfig/ss_libev_configure.sh
+++ ssconfig/ss_libev_configure.sh
@@ -1,26 +1,6 @@
 mkdir shadow
 cd shadow
-yum install git vim -y
-yum install epel-release -y
-yum install gcc gettext autoconf libtool automake make pcre-devel asciidoc xmlto udns-devel libev-devel mbedtls-devel -y
-
-git clone https://github.com/shadowsocks/shadowsocks-libev.git
-cd shadowsocks-libev
-git submodule update --init --recursive
-
-echo 'install libsodium'
-export LIBSODIUM_VER=1.0.15
-wget https://download.libsodium.org/libsodium/releases/libsodium-$LIBSODIUM_VER.tar.gz
-tar xvf libsodium-$LIBSODIUM_VER.tar.gz
-pushd libsodium-$LIBSODIUM_VER
-./configure --prefix=/usr && make
-make install
-popd
-ldconfig
-
-echo 'compile shadowsocks-libev'
-./autogen.sh && ./configure --prefix=/usr && make
-sudo make install
+curl -s https://raw.githubusercontent.com/eshxcmhk/YeildSSH/master/ssconfig/ss_libev_install.sh | sh
 
 mkdir -p /etc/shadowsocks-libev
 curl "https://raw.githubusercontent.com/eshxcmhk/YeildSSH/master/ssconfig/shadowsocks.json" -o "shadowsocks.json"
