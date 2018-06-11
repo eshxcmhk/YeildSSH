7252c4e49704cadebe4cefdf5db42ca72f8c4e0f
eshxcmhk <935057137@qq.com>
2018-06-11 15:09:43 +0800

    Update ss_libev_install.sh

--- ssconfig/ss_libev_install.sh
+++ ssconfig/ss_libev_install.sh
@@ -2,21 +2,31 @@ mkdir shadow
 cd shadow
 yum install git vim -y
 yum install epel-release -y
-yum install gcc gettext autoconf libtool automake make pcre-devel asciidoc xmlto udns-devel libev-devel mbedtls-devel -y
+yum install gcc gettext autoconf libtool automake make pcre-devel asciidoc xmlto c-ares-devel libev-devel -y
 
 git clone https://github.com/shadowsocks/shadowsocks-libev.git
 cd shadowsocks-libev
 git submodule update --init --recursive
 
 echo 'install libsodium'
-export LIBSODIUM_VER=1.0.15
+export LIBSODIUM_VER=1.0.16
 wget https://download.libsodium.org/libsodium/releases/libsodium-$LIBSODIUM_VER.tar.gz
 tar xvf libsodium-$LIBSODIUM_VER.tar.gz
 pushd libsodium-$LIBSODIUM_VER
 ./configure --prefix=/usr && make
 make install
 popd
-ldconfig
+sudo ldconfig
+
+echo 'Installation of MbedTLS'
+export MBEDTLS_VER=2.9.0
+wget https://tls.mbed.org/download/mbedtls-$MBEDTLS_VER-gpl.tgz
+tar xvf mbedtls-$MBEDTLS_VER-gpl.tgz
+pushd mbedtls-$MBEDTLS_VER
+make SHARED=1 CFLAGS=-fPIC
+sudo make DESTDIR=/usr install
+popd
+sudo ldconfig
 
 echo 'compile shadowsocks-libev'
 ./autogen.sh && ./configure --prefix=/usr && make
