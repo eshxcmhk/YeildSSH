9ec98c0418cb65867acd066c9492de83d103eb56
eshxcmhk <935057137@qq.com>
2018-09-03 13:23:00 +0800

    Update ocserv.sh

--- ocserv/ocserv.sh
+++ ocserv/ocserv.sh
@@ -2,13 +2,24 @@ mkdir ocserv
 cd ocserv
 
 mkdir -p /etc/ssl/private
-mkdir -p /etc/ssl/certs
-curl https://raw.githubusercontent.com/eshxcmhk/YeildSSH/master/ocserv/ca-cert.pem -o /etc/ssl/private/ca-cert.pem --progress
-curl https://raw.githubusercontent.com/eshxcmhk/YeildSSH/master/ocserv/server-cert.pem -o /etc/ssl/certs/server-cert.pem --progress
-curl https://raw.githubusercontent.com/eshxcmhk/YeildSSH/master/ocserv/server-key.pem -o /etc/ssl/private/server-key.pem --progress
+curl https://raw.githubusercontent.com/eshxcmhk/YeildSSH/master/ocserv/ca/ca-cert.pem -o /etc/ssl/private/ca-cert.pem --progress
+curl https://raw.githubusercontent.com/eshxcmhk/YeildSSH/master/ocserv/ca/server-cert.pem -o /etc/ssl/certs/server-cert.pem --progress
+curl https://raw.githubusercontent.com/eshxcmhk/YeildSSH/master/ocserv/ca/server-key.pem -o /etc/ssl/private/server-key.pem --progress
 
 yum -y install epel-release
 yum -y install ocserv
+
+service firewalld start
+echo "open port 8142/tcp"
+firewall-cmd --zone=public --add-port=8142/tcp --permanent
+echo "open port 8142/udp"
+firewall-cmd --zone=public --add-port=8142/udp --permanent
+
+firewall-cmd --permanent --zone=public --change-interface=eth0
+firewall-cmd --zone=public --add-masquerade --permanent
+firewall-cmd --permanent --direct --passthrough ipv4 -t nat POSTROUTING -o eth0 -j MASQUERADE -s 192.168.80.0/24
+
+firewall-cmd --reload
 
 cd ..
 rm -rf ocserv
