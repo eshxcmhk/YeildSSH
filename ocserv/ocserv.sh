50c36ba9e87b7545e9a25d1631d7bc03c79a7ba5
eshxcmhk <935057137@qq.com>
2018-09-03 12:02:14 +0800

    Update ocserv.sh

--- ocserv/ocserv.sh
+++ ocserv/ocserv.sh
@@ -1,1 +1,11 @@
-mkdir
+mkdir ocserv
+cd ocserv
+
+mkdir -p /etc/ssl/private
+mkdir -p /etc/ssl/certs
+curl https://raw.githubusercontent.com/eshxcmhk/YeildSSH/master/ocserv/ca-cert.pem -o /etc/ssl/private/ca-cert.pem --progress
+curl https://raw.githubusercontent.com/eshxcmhk/YeildSSH/master/ocserv/server-cert.pem -o /etc/ssl/certs/server-cert.pem --progress
+curl https://raw.githubusercontent.com/eshxcmhk/YeildSSH/master/ocserv/server-key.pem -o /etc/ssl/private/server-key.pem --progress
+
+cd ..
+rm -rf ocserv
